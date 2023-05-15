604dc66ca9b42306e997609a8909f7e5e3d17e79
ferdous <hassanferdous101@gmail.com>
2023-05-15 15:20:17 +0600

    sdfldksfj

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,15 +3,16 @@ import { allPosts, Post } from 'contentlayer/generated';
 
 export const generateStaticParams = async () => {
     const paths = allPosts.map((post: Post) => {
-        return {params: {slug:post.url}}
+        return {slug: post._raw.flattenedPath}
     });
     return paths
 }
 
 
 function Post({params}:{params: {slug: string}}) {
-    console.log(params.slug)
-    const post = allPosts.find((post: Post) => post.url === `/posts/${params.slug}`);
+    const post = allPosts.find((post) => post._raw.flattenedPath === `posts/${params.slug}`)
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
+console.log(params)
   return (
     <div>
         <h1>{post?.title}</h1>
